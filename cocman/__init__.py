0441d59751fe8d5843704e62407b2ce19cf9e12d
Steven Wooding <steve@stevenwooding.com>
2016-03-23 15:42:54 +0000

    fix: Rename file with double underscores
    Also remove the temporary import line. Views
    might be handled in the Angular app. Will just
    have a JSON interface on Flask.

--- cocman/__init__.py
+++ cocman/__init__.py
@@ -7,4 +7,3 @@ app = Flask(__name__)
 # Import modules that have the route() decorator in them.
 # OK to have circular imports here as they are not used in this file.
 # Ref: http://flask.pocoo.org/docs/0.10/patterns/packages/
-import cocman.views
