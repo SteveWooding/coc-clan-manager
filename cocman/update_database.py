95dabdd39ce46ef6f154ae72c279c98c958ad706
Steven Wooding <steve@stevenwooding.com>
2016-03-23 19:47:44 +0000

    feat: Store clan member data in the database

--- cocman/update_database.py
+++ cocman/update_database.py
@@ -1,4 +1,5 @@
-"""Update the database with the given clan tag."""
+"""Update the database with data from the CoC API."""
+import datetime
 
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -9,8 +10,8 @@ from cocman.database_setup import Clan, Member
 from cocman.connect_to_database import connect_to_database
 
 
-
 def update_database(clan_tag):
+    """Update the database with the given clan tag."""
     # Connect to the database
     session = connect_to_database()
 
@@ -38,6 +39,88 @@ def update_database(clan_tag):
     session.add(clan)
     session.commit()
 
-    
+    # Extract the members list data
+    members_data = clan_data['memberList']
 
+    # Loop through the current members of the clan
+    for member_data in members_data:
+        # Create or update member details
+        try:
+            # Get member object to be updated
+            member = (session.query(Member)
+                .filter_by(tag=member_data['tag']).one())
 
+            # Update required fields
+            member.name = member_data['name']
+            member.role = member_data['role']
+            member.exp_level = member_data['expLevel']
+
+        except NoResultFound:
+            # Create a new member object with required data
+            member = Member(
+                clan=clan,
+                tag=member_data['tag'],
+                name=member_data['name'],
+                role=member_data['role'],
+                exp_level=member_data['expLevel']
+            )
+
+        # Update optional member data
+        member.league_id = member_data['league']['id']
+        member.clan_rank = member_data['clanRank']
+        member.previous_clan_rank = member_data['previousClanRank']
+
+        # Update the current and total donations
+        if member.current_donations:
+            if member_data['donations'] != member.current_donations:
+                # Number of donations has changed since last checked,
+                # so update the last active time
+                member.last_active_time = datetime.datetime.now()
+
+                if member_data['donations'] < member.current_donations:
+                    # At the end of the season, donation data is zeroed.
+                    # Just add what we know now to the total donations.
+                    member.total_donations += member_data['donations']
+                else:
+                    # Update the total donations
+                    member.total_donations += (member_data['donations']
+                        - member.current_donations)
+        else:
+            # This is the case of a new member we have no previous data for.
+            member.total_donations = member_data['donations']
+
+        # Update the current donations
+        member.current_donations = member_data['donations']
+
+        # Update the current and total donations received
+        if member.current_donations_rec:
+            if member_data['donationsReceived'] != member.current_donations_rec:
+                # Number of donations has changed since last checked,
+                # so update the last active time
+                member.last_active_time = datetime.datetime.now()
+
+                if (member_data['donationsReceived'] <
+                    member.current_donations_rec):
+                    # At the end of the season, donation data is zeroed.
+                    # Just add what we know now to the total donations.
+                    member.total_donations_rec += member_data[
+                        'donationsReceived']
+                else:
+                    # Update the total donations
+                    member.total_donations_rec += member_data[
+                        'donationsReceived'] - member.current_donations_rec
+        else:
+            # This is the case of a new member we have no previous data for.
+            member.total_donations_rec = member_data['donationsReceived']
+
+        # Update the current donations
+        member.current_donations_rec = member_data['donationsReceived']
+
+        # TODO: Refactor the above two sections of code into a function
+
+        # Commit the member object to the database
+        session.add(member)
+        session.commit()
+
+    # Close the database session
+    session.close()
