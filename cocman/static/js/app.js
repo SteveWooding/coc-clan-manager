c2b8bc67f9260b7fd81b701063be694131c677e1
Steven Wooding <steve@stevenwooding.com>
2016-04-11 13:00:54 +0100

    docs: Add comment about getting the clan data

--- cocman/static/js/app.js
+++ cocman/static/js/app.js
@@ -8,6 +8,7 @@
   app.controller('ClanController', ['$scope', '$http', function($scope, $http) {
     $scope.clanData = {};
 
+    // Get the default clan data
     $http.get('/clandata/mainclan/JSON/').success(function(data) {
       if (data.status === 200) {
         $scope.clanData = data.clanData;
