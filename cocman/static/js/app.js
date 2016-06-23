3cff175f713ed17fa8658e9cf460e4843a288f6c
Steven Wooding <steve@stevenwooding.com>
2016-06-23 17:59:27 +0100

    feat: Display daily active users

--- cocman/static/js/app.js
+++ cocman/static/js/app.js
@@ -6,16 +6,32 @@
   var app = angular.module('clanMan', []);
 
   app.controller('ClanController', ['$scope', '$http', function($scope, $http) {
+    // Define an empty object to store the clan data.
     $scope.clanData = {};
+
+    // Define date variables for calculating daily active users.
+    $scope.numDailyUsers = 0;
+    var timeNow = new Date();
+    var oneDayMs = 86400000;
+    var timeOneDayAgo = timeNow - oneDayMs;
 
     // Get the default clan data
     $scope.update = function() {
       $http.get('/api/clandata/mainclan/JSON/')
         .then(function(response) {
           $scope.clanData = response.data;
+
           // Calculate the total number of wars (used for war win rate, etc)
           $scope.clanData.totalNumWars = ($scope.clanData.warWins +
             $scope.clanData.warTies + $scope.clanData.warLosses);
+
+          // Calculate the number of active users in the past 24 hours
+          $scope.clanData.memberList.forEach(function(member) {
+            if (Date.parse(member.lastActiveTime) > timeOneDayAgo) {
+              $scope.numDailyUsers++;
+            }
+          });
+
         }, function(response) {
           $scope.errorMsg = response.statusText + ': ' + response.data.error;
         });
